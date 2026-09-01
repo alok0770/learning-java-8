893ece1a2c2cc6328639d8256eec489b6c6f2a7b
Alok Rana <alokrana0770@gmail.com>
2026-09-01 19:05:50 +0530

    Add basic of lambda expression

---
+++ src/lambda/Employee.java
@@ -0,0 +1,9 @@
+package lambda;
+
+
+@FunctionalInterface
+public interface Employee{
+
+    String getName();
+
+}
--- src/lambda/LambdaDemo.java
+++ src/lambda/LambdaDemo.java
@@ -3,8 +3,13 @@ package lambda;
 public class LambdaDemo {
     public static void main(String[] args) {
 
+        Employee employee = () ->"Alok";
+        System.out.println("Employee : " + employee.getName());
 
+        Employee  editor = () -> "Rahul";
+
+        System.out.println("Editor : " + editor.getName());
+        }
     }
 
-}
 
